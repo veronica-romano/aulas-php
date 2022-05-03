d5a74e10950a7026cfa476ef3b07f0ecffdd0804
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-05-03 17:42:05 -0300

    Update 00helloworld.php

--- 00helloworld.php
+++ 00helloworld.php
@@ -1,22 +1,19 @@
 <?php
     echo ("Hello World!")
 
-
     function getGrade($a, $b, $c) {
         // your code here
         $score = ($a + $b + $c) / 3;
-        
-        if ($score >=90 && $score <= 100){
-          $letterGrade = 'A';
-        } else if ($score >= 80 && $score < 90){
-          $letterGrade = 'B';
-          } else if ($score >= 70 && $score < 80){
-            $letterGrade = 'C';
-            }else if ($score >= 60 && $score < 70){
-              $letterGrade = 'D';
-               }else{
-                $letterGrade = 'F';
-                 }
-        }
-      }
+        if ($score >= 90 && $score <= 100) {
+          $letterGrade = "A";
+        } elseif ($score >= 80 && $score < 90) {
+              $letterGrade = "B";
+            } elseif ($score >= 70 && $score < 80) {
+                $letterGrade = "C";
+            } elseif ($score >= 60 && $score < 70) {
+                  $letterGrade = "D";
+                } else {
+                    $letterGrade = "F";
+                };
+    }
 ?>
